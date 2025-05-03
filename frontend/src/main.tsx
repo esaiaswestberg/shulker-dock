52f403112dec766153479b9e03d5996029da6eb5
Esaias Westberg <esaias@westbergs.se>
2025-05-03 11:05:39 +0200

    Integrate ThemeProvider component for dark/light theme handling in main app layout and update rendering structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
 import './index.css'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { ThemeProvider } from './components/theme-provider'
 
 const queryClient = new QueryClient()
 const router = createRouter({routeTree})
@@ -17,7 +18,9 @@ declare module '@tanstack/react-router' {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme='dark' storageKey='shulker-dock-theme'>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
